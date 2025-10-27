ede2270933a947d94b6b83d85226805ec85b534b
CarlosMEsteban <estgarca@gmail.com>
2025-10-27 07:26:47 +0100

    Productos de pedido en orden de pedidos

--- src/app/orden-pedidos-hijo/orden-pedidos-hijo.ts
+++ src/app/orden-pedidos-hijo/orden-pedidos-hijo.ts
@@ -4,6 +4,8 @@ import { OrdenPedidosProductosHijo } from '../orden-pedidos-productos-hijo/orden
 import { ProductosDePedidoModel } from '../productos-de-pedido/productosDePedido.model';
 import { ProductoDePedidoService } from '../producto-de-pedido/producto-de-pedido.service';
 import { NgFor } from '@angular/common';
+import { ProductoService } from '../producto/producto.service';
+import { ProductoModel } from '../producto/producto.model';
 
 @Component({
   selector: 'app-orden-pedidos-hijo',
@@ -14,15 +16,17 @@ import { NgFor } from '@angular/common';
 export class OrdenPedidosHijo 
 {
   @Input() pedido!: PedidoModel;
+  @Input() lProductos!: ProductoModel[];
 
   lProductosDePedido: ProductosDePedidoModel[] = [];
 
   productoDePedidoServicio: ProductoDePedidoService;
+  productoServicio: ProductoService;
 
-  constructor(productoDePedidoServicio: ProductoDePedidoService)
+  constructor(productoDePedidoServicio: ProductoDePedidoService, productoServicio: ProductoService)
   {
     this.productoDePedidoServicio = productoDePedidoServicio;
-
+    this.productoServicio = productoServicio;
   }
 
   ngOnInit()
@@ -42,8 +46,9 @@ export class OrdenPedidosHijo
     }
     try {
       this.lProductosDePedido = await this.productoDePedidoServicio.obtenerProductosDePedidoPorPedidoId(this.pedido.id);
-      // opcional: asignar pedidoId a cada productoDePedido si hace falta
-      this.lProductosDePedido.forEach(p => (p.pedidoId = this.pedido.id));
+      // A cada producto le ponemos su nombre de producto
+      this.lProductosDePedido.forEach(pdp =>(pdp.nombreProducto = this.lProductos.find(p => p.cProductoId == pdp.poductoId)?.getNombre() || ""))
+      
     } catch (err) {
       console.error('Error cargando productos del pedido', err);
       this.lProductosDePedido = [];
@@ -51,4 +56,6 @@ export class OrdenPedidosHijo
 
     console.log("Pedido: " + this.pedido.id+ " tiene "+  this.lProductosDePedido.length + " productos")
  }
+
+ 
 }
