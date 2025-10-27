aea29122a22ccff194fbd70ae04cc2046d1e4124
CarlosMEsteban <estgarca@gmail.com>
2025-10-27 07:35:28 +0100

    Productos de pedido en orden de pedidos

--- src/app/orden-pedidos-hijo/orden-pedidos-hijo.ts
+++ src/app/orden-pedidos-hijo/orden-pedidos-hijo.ts
@@ -47,7 +47,16 @@ export class OrdenPedidosHijo
     try {
       this.lProductosDePedido = await this.productoDePedidoServicio.obtenerProductosDePedidoPorPedidoId(this.pedido.id);
       // A cada producto le ponemos su nombre de producto
-      this.lProductosDePedido.forEach(pdp =>(pdp.nombreProducto = this.lProductos.find(p => p.cProductoId == pdp.poductoId)?.getNombre() || ""))
+      this.lProductosDePedido.forEach(pdp =>{
+        let producto = this.lProductos.find(p => p.cProductoId == pdp.poductoId);
+        if (producto == undefined)
+          pdp.nombreProducto = "Nombre de producto no encontrado";
+        else
+        {
+          pdp.nombreProducto = producto.nombre;
+          pdp.tengo = producto.tengo;
+        }
+      })
       
     } catch (err) {
       console.error('Error cargando productos del pedido', err);
